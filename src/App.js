b904112af1269303aa56a5b9e963181a254b8bb9
ugulavaleri <leri.ugulava1@gmail.com>
2023-05-11 18:15:26 +0400

    changed logic

--- src/App.js
+++ src/App.js
@@ -10,40 +10,45 @@ import icon2 from "./images/Vector (2).png";
 import icon3 from "./images/Vector (3).png";
 import icon4 from "./images/Vector.png";
 
+const data = [
+    {
+        imgBg: Bg1,
+        img: icon1,
+        title: " Error",
+        text: "Order Placed Successfully. You can check order delivery status.",
+        color: "#EB5757",
+    },
+    {
+        imgBg: Bg2,
+        img: icon2,
+        title: "Warning",
+        text: "Order Placed Successfully. You can check order delivery status.",
+        color: "#F2C94C",
+    },
+    {
+        imgBg: Bg3,
+        img: icon3,
+        title: "Info",
+        text: "Order Placed Successfully. You can check order delivery status.",
+        color: "#5458F7",
+    },
+    {
+        imgBg: Bg4,
+        img: icon4,
+        title: "Success",
+        text: "Order Placed Successfully. You can check order delivery status.",
+        color: "#00CC99",
+    },
+];
+
+const types = ["Error", "Warning", "Info", "Success"];
+const type = "Success";
+
 function App() {
     return (
         <>
             <Notification
-                imgBg={Bg1}
-                img={icon1}
-                title="Success"
-                text="Order Placed Successfully. You can check order delivery status."
-                color=" #EB5757"
-                key={1}
-            />
-            <Notification
-                imgBg={Bg2}
-                img={icon2}
-                title="Error"
-                text="Order Placed Successfully. You can check order delivery status."
-                color=" #F2C94C"
-                key={2}
-            />
-            <Notification
-                imgBg={Bg3}
-                img={icon3}
-                title="Warning"
-                text="Order Placed Successfully. You can check order delivery status."
-                color=" #5458F7"
-                key={3}
-            />
-            <Notification
-                imgBg={Bg4}
-                img={icon4}
-                title="Info"
-                text="Order Placed Successfully. You can check order delivery status."
-                color=" #00CC99"
-                key={4}
+                type={types.includes(type) ? data[types.indexOf(type)] : null}
             />
         </>
     );
--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -2,14 +2,14 @@ import "./Notifications.css";
 
 const Item = (props) => {
     return (
-        <div className="wrapper" style={{ "--bg-color": props.color }}>
+        <div className="wrapper" style={{ "--bg-color": props.type.color }}>
             <div className="imageDiv">
-                <img src={props.imgBg} alt="bgimage" />
-                <img src={props.img} alt="icon" className="img" />
+                <img src={props.type.imgBg} alt="bgimage" />
+                <img src={props.type.img} alt="icon" className="img" />
             </div>
             <div className="textDiv">
-                <h2>{props.title}</h2>
-                <p>{props.text}</p>
+                <h2>{props.type.title}</h2>
+                <p>{props.type.text}</p>
             </div>
         </div>
     );
